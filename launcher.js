d0e9e63e465505a8617b3b862ff349b650205a38
donkihot7 <donkihot24@mail.ru>
2018-09-07 11:44:37 +0300

    загружаю начало проекта

--- launcher.js
+++ launcher.js
@@ -1,4 +1,27 @@
-launcher.js
-Разработка
-и проверка
-пробуем без Санька
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// ... the starter code you pasted ...
+
+const domContainer = document.querySelector('#launcher.js');
+ReactDOM.render(e(LikeButton), domContainer);
